128a785abc39806333997dd86cd47ced13722070
Gints Bērziņš <gintsb12@gmail.com>
2018-10-14 20:49:44 +0300

    removed ajax console log email

--- resources/js/ajax.js
+++ resources/js/ajax.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     $("#email").change(function(){
         let email = $("#email").val();
         let token = $('meta[name="csrf-token"]').attr('content');
-        console.log(email);
         $.ajax({
             type: 'POST',
             url: '/check_email',
